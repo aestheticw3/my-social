2bdca377d42c418fb444cc8e805f3a6ed1bf54fa
Danila Kudashov <kudashovdanila@gmail.com>
2022-10-15 08:34:46 +0700

    add message button

--- src/components/Messages/ChatMessages.jsx
+++ src/components/Messages/ChatMessages.jsx
@@ -1,16 +1,37 @@
+import React from "react";
 import styles from "./ChatMessages.module.css";
 import ChatMessage from "./ChatMessage";
-import ChatTextarea from "./ChatTextarea";
 
 const ChatMessages = (props) => {
   let chatMessagesElements = props.chatMessagesData.map((chatMessage) => (
     <ChatMessage message={chatMessage.message} />
   ));
 
+  let messageText = React.createRef();
+
+  let sendMessage = () => {
+    let text = messageText.current.value;
+    alert(text);
+  };
+
   return (
     <div className={styles.chatMessages}>
       {chatMessagesElements}
-      <ChatTextarea />
+      <textarea
+        ref={messageText}
+        className={styles.textfield}
+        placeholder="Your Message..."
+        type="text"
+        autocapitalize="none"
+        autocomplete="off"
+        autocorrect="off"
+        name="message-text"
+        tabindex="0"
+        spellcheck="false"
+      ></textarea>
+      <button onClick={sendMessage} className={styles.btn}>
+        Send
+      </button>
     </div>
   );
 };
